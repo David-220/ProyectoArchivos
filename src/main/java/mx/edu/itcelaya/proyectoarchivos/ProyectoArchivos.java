1b8a889f74f33a09d5e60eb240b043283eb965da
David-220 <davidarh220@gmail.com>
2025-11-19 20:22:46 -0600

    commit inicial

---
+++ src/main/java/mx/edu/itcelaya/proyectoarchivos/ProyectoArchivos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.edu.itcelaya.proyectoarchivos;
+
+/**
+ *
+ * @author John Doe
+ */
+public class ProyectoArchivos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
